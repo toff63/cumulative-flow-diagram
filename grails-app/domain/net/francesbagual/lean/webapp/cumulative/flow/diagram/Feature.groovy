6e2d103da811a0e6d6c6f38cf0ff147173335ebf
Christophe Marchal <toff63@gmail.com>
2012-03-04 12:57:04 -0300

    Added properties to Feature domain class

--- grails-app/domain/net/francesbagual/lean/webapp/cumulative/flow/diagram/Feature.groovy
+++ grails-app/domain/net/francesbagual/lean/webapp/cumulative/flow/diagram/Feature.groovy
@@ -3,5 +3,12 @@ package net.francesbagual.lean.webapp.cumulative.flow.diagram
 class Feature {
 
     static constraints = {
+		name blank: false
     }
+	
+	String name;
+	String externalUrl;
+	String description;
+	Date start;
+	Date end;
 }
